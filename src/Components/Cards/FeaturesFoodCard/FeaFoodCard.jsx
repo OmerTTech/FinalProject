12828fe146c7fafcc0aa9675d1c4adc3ce9f13d3
HyperOmerJ <omer.tapdiqov@gmail.com>
2024-07-03 18:02:54 +0400

    small fixes

--- src/Components/Cards/FeaturesFoodCard/FeaFoodCard.jsx
+++ src/Components/Cards/FeaturesFoodCard/FeaFoodCard.jsx
@@ -4,32 +4,36 @@ import Buttonfade from "../../ButtonFade/filled/Buttonfadefill";
 
 const FeaFoodCard = () => {
   return (
-    <div className="col-3 border border-danger rounded">
-      <p
-        className="position-relative d-flex justify-content-center bg-primary mb-0"
-        style={{ height: "50%" }}
-      >
+    <div
+      class="card col-3 card-border rounded"
+      style={{ width: "18rem" }}
+    >
+      <div className="bg-danger p-0 py-2 mt-2 d-flex justify-content-center">
+      <div className="w-50 h-100 p-2 bg-white rounded-circle">
         <img
-          style={{ clipPath:"circle(50%)", width: "50%", objectFit: "contain" }}
+          class="card-img-top rounded-circle"
           src="https://swigo.dexignzone.com/xhtml/assets/images/gallery/small/pic1.jpg"
-          alt=""
+          alt="Card image cap"
         />
-      </p>
-      <h3 className="h3 text-center p-0 m-0">Pizza</h3>
-      <p className="text-secondary text-center p-0 m-0">
-        Mediterranean, Mixed, Margarita, Sausage.
-      </p>
-      <p className="h5 text-danger fw-bold text-center w-100 mb-3">
-        ${`55.00`}
-      </p>
-      <p className="p-1 py-2 d-flex justify-content-center">
-        <Buttonfade
-          text={"Add to Card"}
-          fontSize={"0.85rem"}
-          W={"120px"}
-          H="40px"
-        />
-      </p>
+      </div>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title h3 text-center">Pizza</h5>
+        <p class="card-text text-secondary text-center mb-1" style={{filter: "drop-shadow(0px 0px 0.00001px #555)"}}>
+          Mediterranean, Mixed, Margarita, Sausage.
+        </p>
+        <h4 className="h4 text-danger fw-bold text-center w-100 mb-3">
+          ${`55.00`}
+        </h4>
+        <p className="p-1 py-2 mb-0 d-flex justify-content-center">
+          <Buttonfade
+            text={"Add to Card"}
+            fontSize={"0.85rem"}
+            W={"120px"}
+            H="40px"
+          />
+        </p>
+      </div>
     </div>
   );
 };
